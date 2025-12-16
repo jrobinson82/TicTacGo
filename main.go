fd3d052d571460a9306f78d6985a277f5e838e0e
John Robinson <jarobinson1c@gmail.com>
2025-12-16 13:38:14 -0600

    Created game board object

--- main.go
+++ main.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 )
 
+type GameSquare struct {
+	player int
+	move   string
+}
+
 func main() {
 	var input string
 	gameOn := true
+	var gameBoard [9]GameSquare
+
+	// Clear Game Board
+	for i := 0; i < 9; i++ {
+		gameBoard[i].player = 0
+		gameBoard[i].move = "[-]"
+
+		fmt.Printf("Game | Square: %d Player: %d Move: %v \n", i+1, gameBoard[i].player, gameBoard[i].move)
+	}
 
 	fmt.Println(`Type "q" and Enter to end game:`)
 
 	// Main game loop
-	for gameOn == true {
+	for gameOn {
 		fmt.Println("Game: On> ")
 		fmt.Scanln(&input)
 
