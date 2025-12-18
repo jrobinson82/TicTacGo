4c847e5cb566e969400cd4617681c17d08a31a60
John Robinson <jarobinson1c@gmail.com>
2025-12-18 14:16:16 -0600

    Added clear and render board functions

--- main.go
+++ main.go
@@ -9,24 +9,48 @@ type GameSquare struct {
 	move   string
 }
 
+func clearGameBoard(gameBoard []GameSquare) {
+	for i := 0; i < 9; i++ {
+		gameBoard[i].player = 0
+		gameBoard[i].move = "[ ]"
+
+	}
+}
+
+func renderGameBoard(gameBoard []GameSquare) {
+	i := 0
+
+	fmt.Println("")
+
+	for r := 0; r < 3; r++ {
+		for c := 0; c < 3; c++ {
+			fmt.Printf("%s", gameBoard[i].move)
+			i++
+		}
+		fmt.Println("")
+	}
+
+	fmt.Println("")
+}
+
 func main() {
 	var input string
 	gameOn := true
+	playerTurn := 1
 	var gameBoard [9]GameSquare
 
-	// Clear Game Board
-	for i := 0; i < 9; i++ {
-		gameBoard[i].player = 0
-		gameBoard[i].move = "[-]"
-
-		fmt.Printf("Game | Square: %d Player: %d Move: %v \n", i+1, gameBoard[i].player, gameBoard[i].move)
-	}
-
 	fmt.Println(`Type "q" and Enter to end game:`)
+
+	clearGameBoard(gameBoard[:])
 
 	// Main game loop
 	for gameOn {
 		fmt.Println("Game: On> ")
+
+		renderGameBoard(gameBoard[:])
+
+		fmt.Printf(`Ready Player %d (Type "x","o" and Enter): `, playerTurn)
+
 		fmt.Scanln(&input)
 
 		if input == "q" {
