84050b607cdbe6f6460916b5673d8317171c720e
John Robinson <jarobinson1c@gmail.com>
2025-12-19 16:22:47 -0600

    Playable game with minimal constraints

--- main.go
+++ main.go
@@ -2,29 +2,33 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-type GameSquare struct {
-	player int
-	move   string
-}
-
-func clearGameBoard(gameBoard []GameSquare) {
+func clearGameBoard(gameBoard []string) {
 	for i := 0; i < 9; i++ {
-		gameBoard[i].player = 0
-		gameBoard[i].move = "[ ]"
-
+		gameBoard[i] = "[ ]"
 	}
 }
 
-func renderGameBoard(gameBoard []GameSquare) {
+func renderGameBoard(gameBoard []string, showCellNumbers ...bool) {
 	i := 0
+	cellNumbers := false
+
+	if len(showCellNumbers) > 0 {
+		cellNumbers = showCellNumbers[0]
+	}
 
 	fmt.Println("")
 
 	for r := 0; r < 3; r++ {
 		for c := 0; c < 3; c++ {
-			fmt.Printf("%s", gameBoard[i].move)
+			if !cellNumbers {
+				fmt.Printf("%s", gameBoard[i])
+			} else {
+				fmt.Printf("%d%s ", i+1, gameBoard[i])
+			}
+
 			i++
 		}
 		fmt.Println("")
@@ -33,15 +37,37 @@ func renderGameBoard(gameBoard []GameSquare) {
 	fmt.Println("")
 }
 
+func turnToggle(turn *string) {
+	if *turn == "x" {
+		*turn = "o"
+	} else {
+		*turn = "x"
+	}
+}
+
 func main() {
 	var input string
 	gameOn := true
-	playerTurn := 1
-	var gameBoard [9]GameSquare
+	validInput := false
+	playerTurn := "x"
+	var gameBoard [9]string
 
-	fmt.Println(`Type "q" and Enter to end game:`)
+	fmt.Println()
+	fmt.Println("--------------------------------------")
+	fmt.Println("****** Welcome To TIC TAC [GO]! ******")
+	fmt.Println("--------------------------------------")
+	fmt.Println("Instructions:")
+	fmt.Println(`When it's you turn type a cell number (1-9)`)
+	fmt.Println(`and press Enter to mark a square.`)
+	fmt.Println(`Type "q" to quit game.`)
+	fmt.Println(`Here are the cell numbers...`)
 
 	clearGameBoard(gameBoard[:])
+	renderGameBoard(gameBoard[:], true)
+
+	fmt.Println("--------------------------------------")
+	fmt.Println("--------------------------------------")
+	fmt.Println()
 
 	// Main game loop
 	for gameOn {
@@ -49,15 +75,38 @@ func main() {
 
 		renderGameBoard(gameBoard[:])
 
-		fmt.Printf(`Ready Player %d (Type "x","o" and Enter): `, playerTurn)
-
+		fmt.Printf(`Ready Player "%s"> `, playerTurn)
 		fmt.Scanln(&input)
 
-		if input == "q" {
-			fmt.Println("Game: Off")
-			break
+		num, err := strconv.Atoi(input)
+
+		if err != nil {
+			num = -1
 		}
 
-		fmt.Println("Input: ", input)
+		if input == "q" {
+			validInput = true
+			fmt.Println("Game: Off<")
+			break
+		} else {
+			if err != nil {
+				fmt.Printf(`"%s" is an invalid command.`, input)
+			} else {
+				if num >= 1 && num <= 9 {
+					validInput = true
+					gameBoard[num-1] = "[" + playerTurn + "]"
+				} else {
+					fmt.Printf(`"%d" is out of range. Please select a number between 1 and 9.`, num)
+				}
+			}
+		}
+
+		fmt.Println()
+		fmt.Println("#####################################")
+		fmt.Println()
+
+		if validInput {
+			turnToggle(&playerTurn)
+		}
 	}
 }
